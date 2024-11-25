4ec6c92df8730e0322b925ba14a05734256d2ff8
Arthi Arumugam <62213284+arthi-arumugam99@users.noreply.github.com>
2024-11-25 08:52:37 +0000

    Update send_whatsapp.py

--- send_whatsapp.py
+++ send_whatsapp.py
@@ -1,10 +1,10 @@
 from twilio.rest import Client
 
 # Twilio API credentials
-account_sid = 'AC53f642d38652002ed88162b8378bee60'
-auth_token = '4e3fd4ce29b03064ffdef7dbd174f4f8'
+account_sid = ''
+auth_token = ''
 twilio_whatsapp_number = 'whatsapp:+14155238886'  # Twilio sandbox WhatsApp number
-recipient_whatsapp_number = 'whatsapp:+919677667794'  # Replace with your WhatsApp number in E.164 format (e.g., whatsapp:+14155552671)
+recipient_whatsapp_number = ''  # WhatsApp number in E.164 format (e.g., whatsapp:+14155552671)
 
 # Initialize Twilio client
 client = Client(account_sid, auth_token)
